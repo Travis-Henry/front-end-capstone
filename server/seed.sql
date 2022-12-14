bbf6c5e7b7d3266fc0da54b2eff4ffb87469e4db
Dustin Swartzbaugh <dustin.swartzbaugh@gmail.com>
2022-12-14 15:39:57 -0500

    adding data to movies table

--- server/seed.sql
+++ server/seed.sql
@@ -1,1 +1,19 @@
-INSERT INTO movies (movieName) VALUES ('The Count of Monte Cristo'), ('Pacific Rim'), ('Book of Eli'), ('Intersteller'), ('Glass')
+INSERT INTO movies 
+(movieName, releaseDate, studio, language, budget, sales, poster) 
+VALUES ('The Count of Monte Cristo'), 
+('Pacific Rim'), 
+('Book of Eli'), 
+('Intersteller'), 
+('Glass')
+
+INSERT INTO movies (plot)
+VALUES (random_paragraph(50, 500))
+
+movieName varchar(50),
+  releaseDate varchar(20),
+  studio varchar(50),
+  language varchar(25),
+  budget int,
+  sales int
+  plot text,
+  poster text
