82a03d5127865bb85679c66aaa33acdb1f5ad84b
Travis-Henry <travis.henry2016@gmail.com>
2022-12-14 15:21:37 -0700

    added seed for cast table

--- server/seed.sql
+++ server/seed.sql
@@ -17,3 +17,32 @@ movieName varchar(50),
   sales int
   plot text,
   poster text
+
+
+
+
+
+
+
+
+
+//Cast info for Count of Monte Cristo
+  INSERT INTO cast (name, character, movie_id, photo)
+VALUES ('Jim Caviezel', 'Edmond', 1, 'https://m.media-amazon.com/images/M/MV5BMTc2Nzk0MTY4MV5BMl5BanBnXkFtZTcwMjA3NTY5Mg@@._V1_QL75_UY207_CR3,0,140,207_.jpg'),
+       ('Guy Pearce', 'Fernand', 1, 'https://m.media-amazon.com/images/M/MV5BMTgyNzc2NzY3Nl5BMl5BanBnXkFtZTgwNTMzMzAwMjE@._V1_QL75_UX140_CR0,1,140,207_.jpg'),
+       ('Christopher Adamson', 'Maurice', 1, 'https://m.media-amazon.com/images/M/MV5BMTAwMDAyNTc2ODleQTJeQWpwZ15BbWU3MDk5NTQ0MDg@._V1_QL75_UY207_CR2,0,140,207_.jpg'),
+       ('JB Blanc', 'Luigi Vampa', 1, 'https://m.media-amazon.com/images/M/MV5BMGJmOTM5MjQtMWUzOC00NzUxLThmMTctZTc2ZWVlNzkxYjg4XkEyXkFqcGdeQXVyMTM2MzA2NA@@._V1_QL75_UX140_CR0,1,140,207_.jpg'),
+       ('Guy Carleton', 'Mansion Owner', 1, 'https://m.media-amazon.com/images/M/MV5BYzYwZTdlYWItZmMyNi00ZmRkLThhYTItYWNhYzI4ZDU3MmU5XkEyXkFqcGdeQXVyMTExMTc3NzU5._V1_QL75_UY207_CR3,0,140,207_.jpg'),
+       ('Barry Cassin', 'Old Man Dantes', 1),
+       ('Henry Cavill', 'Albert Mondego', 1, 'https://m.media-amazon.com/images/M/MV5BODI0MTYzNTIxNl5BMl5BanBnXkFtZTcwNjg2Nzc0NA@@._V1_QL75_UY207_CR17,0,140,207_.jpg'),
+       ('Briana Corrigan', 'Casino Prostitute', 1),
+       ('Brendan Costello', 'Viscount', 1),
+       ('Mairead Devlin', 'Partygoer No. 2(as Maireid Devlin)', 1),
+       ('Dagmara Dominczyk', 'Mercedes', 1, 'https://m.media-amazon.com/images/M/MV5BMTIzOTMzMTg5NV5BMl5BanBnXkFtZTYwODIzMDEz._V1_QL75_UY207_CR13,0,140,207_.jpg'),
+       ('Stella Feeley', 'Partygoer No. 3', 1),
+       ('James Frain', 'Villefort', 1, 'https://m.media-amazon.com/images/M/MV5BYWY3NTc5YWQtN2JjZS00Yjc2LThlZTEtMTQwNmY5MTQxY2QzXkEyXkFqcGdeQXVyNDcyMTkwOQ@@._V1_QL75_UY207_CR31,0,140,207_.jpg'),
+       ('Patrick Godfrey', 'Morrell', 1, 'https://m.media-amazon.com/images/M/MV5BMTQwNzU0MTUxMl5BMl5BanBnXkFtZTcwNjc4OTkwOA@@._V1_QL75_UX140_CR0,12,140,207_.jpg'),
+       ('Luis Guzmán', 'Jacopo(as Luis Guzman)', 1, 'https://m.media-amazon.com/images/M/MV5BNzYyNjIyOTU2NF5BMl5BanBnXkFtZTcwODk3ODcxMw@@._V1_QL75_UY207_CR1,0,140,207_.jpg'),
+       ('Joe Hanley', 'Partygoer No. 1', 1),
+       ('Richard Harris', 'Abbe Faria', 1, 'https://m.media-amazon.com/images/M/MV5BMTgzNTA5ODg1NV5BMl5BanBnXkFtZTcwMDU3MTU5Mw@@._V1_QL75_UY207_CR8,0,140,207_.jpg'),
+       ('Katherine Holme', 'Julianne', 1);
