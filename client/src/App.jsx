d30f3549d89fb65ee62e3af6e3b04a930fe69544
Travis-Henry <travis.henry2016@gmail.com>
2022-12-15 13:34:23 -0700

    Added initial get request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Primary from "./components/Primary";
 import Details from "./components/Details";
@@ -6,8 +6,18 @@ import MoreToExplore from "./components/MoreToExplore";
 import Cast from "./components/Cast";
 import Reviews from "./components/Reviews";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
 function App() {
   const [count, setCount] = useState(0);
+  const [currentMovie, setCurrentMovie] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/movie/1")
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="App">
