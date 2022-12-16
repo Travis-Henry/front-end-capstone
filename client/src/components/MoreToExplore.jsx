a7970ea47a5e6c8528adc5576dfb1cc9e48bdec6
BAlimbekov <alimbekovbekmambet@gmail.com>
2022-12-16 16:57:56 -0600

    added random pictures

--- client/src/components/MoreToExplore.jsx
+++ client/src/components/MoreToExplore.jsx
@@ -1,13 +1,21 @@
 import "./MoreToExplore.css";
 import { MdChevronRight, MdChevronLeft } from "react-icons/md";
 import Star from "./Star";
+import { CiCircleInfo } from "react-icons/ci";
 import Button from 'react-bootstrap/Button';
 
 
 function MoreToExplore(props) {
 
   console.log("current movie", props.currentMovie)
-  const slides = [1, 2, 3, 4, 5, 6, 7, 8];
+  const slides = [{image:'https://picsum.photos/200/299', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/301', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/302', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/303', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/304', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/306', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/307', title:"The Count of Monte Cristo"},
+  {image:'https://picsum.photos/200/305', title:"The Count of Monte Cristo"}];
 
   const slideLeft = () => {
     var slider = document.getElementById("slider");
@@ -21,23 +29,28 @@ function MoreToExplore(props) {
 
   return (
     <div id="main-slider-container">
-      <MdChevronLeft size={40} className="slider-icon left" onClick={slideLeft} />
+      <MdChevronLeft size={50} className="slider-icon left" onClick={slideLeft} />
       <div id="slider">
         {slides.map((slide, index) => {
           return (
             <div className="slider-card">
-              <div className="slider-card-image"></div>
+              <img className="slider-card-image" src={slide.image}></img>
+              <div></div>
               <Star/>
-              <p>Movie Title</p>
-              <Button variant="secondary">Watch options</Button>
+              <p>{slide.title}</p>
+              <Button className="card-button" variant="secondary">Watch options</Button>
+              <div className="card-info-div">
+                <CiCircleInfo size={(25)} className="card-info"/>
+              </div>
             </div>
           )
         })
         }
       </div>
-      <MdChevronRight size={40} className="slider-icon right" onClick={slideRight} />
+      <MdChevronRight size={50} className="slider-icon right" onClick={slideRight} />
     </div>
   );
 }
 
 export default MoreToExplore;
+
