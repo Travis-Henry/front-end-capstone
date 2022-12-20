bff1c9616d1c07fe741cb490b02939a8cb734517
Dustin Swartzbaugh <dustin.swartzbaugh@gmail.com>
2022-12-20 16:40:23 -0500

    fixed author, date, and rating to match up with db

--- client/src/components/reviews/Featured.jsx
+++ client/src/components/reviews/Featured.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useContext } from 'react'
+import React, { useState , createContext, useContext } from 'react'
 import UpVote from './UpVote'
 import { listContextStates } from '../../App'
+import { reviewContext } from './ReviewsBody'
 import DownVote from './DownVote'
+
+
 
 export default function Featured() {
   const [currentMovie] = useContext(listContextStates)
-  const i = Math.floor(Math.random() * 100)
-  console.log(currentMovie)
+  const i = useContext(reviewContext)
   const title = currentMovie.reviews[i].title;
   const review = currentMovie.reviews[i].content;
-  const sum = currentMovie.reviews.map(r => r.rating).reduce((a, b) => a + b);
-  const avg = sum/currentMovie.reviews.length
-  const usableAvg = parseFloat(avg.toFixed(1))
- 
-
-
+  const rating = currentMovie.reviews[i].rating;
+  // const sum = currentMovie.reviews.map(r => r.rating).reduce((a, b) => a + b);
+  // const avg = sum/currentMovie.reviews.length
+  // const usableAvg = parseFloat(avg.toFixed(1))
  
 
   return (
@@ -31,7 +31,7 @@ export default function Featured() {
             className="star-icon"
             viewBox="0 0 24 24">
             <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
-          </svg>{usableAvg}<span className="max-rating">/10</span>
+          </svg>{rating}<span className="max-rating">/10</span>
         </span>        
       </div>
       <div className="featured-review--title-main">
